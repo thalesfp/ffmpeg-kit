06a3c1bcf5ec60b46868cfd65fb9cf58b88988ca
Thales Figueredo Pereira <thalesfp@gmail.com>
2025-09-18 19:53:55 -0700

    Release version 7.0

--- Package.swift
+++ Package.swift
@@ -19,42 +19,42 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "FFmpegKit",
-            url: "https://github.com/thalesfp/ffmpeg-kit/releases/download/v6.0/ffmpegkit.xcframework.zip",
+            url: "https://github.com/thalesfp/ffmpeg-kit/releases/download/v7.0/ffmpegkit.xcframework.zip",
             checksum: "PLACEHOLDER_CHECKSUM_FFMPEGKIT"
         ),
         .binaryTarget(
             name: "libavcodec",
-            url: "https://github.com/thalesfp/ffmpeg-kit/releases/download/v6.0/libavcodec.xcframework.zip",
+            url: "https://github.com/thalesfp/ffmpeg-kit/releases/download/v7.0/libavcodec.xcframework.zip",
             checksum: "PLACEHOLDER_CHECKSUM_LIBAVCODEC"
         ),
         .binaryTarget(
             name: "libavdevice",
-            url: "https://github.com/thalesfp/ffmpeg-kit/releases/download/v6.0/libavdevice.xcframework.zip",
+            url: "https://github.com/thalesfp/ffmpeg-kit/releases/download/v7.0/libavdevice.xcframework.zip",
             checksum: "PLACEHOLDER_CHECKSUM_LIBAVDEVICE"
         ),
         .binaryTarget(
             name: "libavfilter",
-            url: "https://github.com/thalesfp/ffmpeg-kit/releases/download/v6.0/libavfilter.xcframework.zip",
+            url: "https://github.com/thalesfp/ffmpeg-kit/releases/download/v7.0/libavfilter.xcframework.zip",
             checksum: "PLACEHOLDER_CHECKSUM_LIBAVFILTER"
         ),
         .binaryTarget(
             name: "libavformat",
-            url: "https://github.com/thalesfp/ffmpeg-kit/releases/download/v6.0/libavformat.xcframework.zip",
+            url: "https://github.com/thalesfp/ffmpeg-kit/releases/download/v7.0/libavformat.xcframework.zip",
             checksum: "PLACEHOLDER_CHECKSUM_LIBAVFORMAT"
         ),
         .binaryTarget(
             name: "libavutil",
-            url: "https://github.com/thalesfp/ffmpeg-kit/releases/download/v6.0/libavutil.xcframework.zip",
+            url: "https://github.com/thalesfp/ffmpeg-kit/releases/download/v7.0/libavutil.xcframework.zip",
             checksum: "PLACEHOLDER_CHECKSUM_LIBAVUTIL"
         ),
         .binaryTarget(
             name: "libswresample",
-            url: "https://github.com/thalesfp/ffmpeg-kit/releases/download/v6.0/libswresample.xcframework.zip",
+            url: "https://github.com/thalesfp/ffmpeg-kit/releases/download/v7.0/libswresample.xcframework.zip",
             checksum: "PLACEHOLDER_CHECKSUM_LIBSWRESAMPLE"
         ),
         .binaryTarget(
             name: "libswscale",
-            url: "https://github.com/thalesfp/ffmpeg-kit/releases/download/v6.0/libswscale.xcframework.zip",
+            url: "https://github.com/thalesfp/ffmpeg-kit/releases/download/v7.0/libswscale.xcframework.zip",
             checksum: "PLACEHOLDER_CHECKSUM_LIBSWSCALE"
         ),
     ]
